ebff1037f5d5cf493d7a235c391bd5622e0e16c2
Gloitel <zafaryab.khan@gloitel.in>
2024-02-02 17:50:59 +0530

    add chnages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // if you don't need to debug service worker in dev, you can set disable:
+  disable: process.env.NODE_ENV === 'development',
+});
+
+module.exports = withPWA({
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+});
--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import '@/app/ui/global.css';
 import { Inter } from 'next/font/google';
 
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Conatct Page',
-  description: 'This page is all about containg  information',
-};
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
