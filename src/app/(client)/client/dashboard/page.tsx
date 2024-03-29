43b1cd0992a7ad350b0841b9b9cd27e8e91b0f9a
rishabh <rishabh.nirmalkar@gloitel.in>
2024-03-29 10:56:07 +0530

    client page

--- src/app/(client)/client/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import React from 'react';
-import { withAuthentication } from '@/hocs';
-const Dashboard = () => {
-  return <div>Client hero--------</div>;
-};
-
-export default withAuthentication(Dashboard);
