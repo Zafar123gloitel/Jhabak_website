a8d6126ee9dbc684bdd482c006c343c6d4ad4309
rishabh <rishabh.nirmalkar@gloitel.in>
2024-04-03 10:33:21 +0530

    add changes

--- src/app/(client)/client/day_calls/page.tsx
+++ src/app/(client)/client/day_calls/page.tsx
@@ -1,10 +1,56 @@
-import React from 'react';
-import Day_calls from '@/components/client/plans/day_calls/Day_calls';
+'use client';
+import React, { useState } from 'react';
+import Equity_trading from '@/components/client/plans/day_calls/Equity_Trading/Equity_Trading';
+import Option_trading from '@/components/client/plans/day_calls/Option_Trading/Option_trading';
+import style from './call.module.scss';
+import TabComponent from '@/components/TabComponents';
+
+interface ITabData {
+  _id: number;
+  name: string;
+  alt: string;
+}
+
+const tabData: ITabData[] = [
+  {
+    _id: 3121,
+    name: 'equity_trading',
+    alt: 'Equity Trading',
+  },
+  {
+    _id: 3122,
+    name: 'option_trading',
+    alt: 'Options Trading',
+  },
+  {
+    _id: 3123,
+    name: 'mcx',
+    alt: 'MCX',
+  },
+];
 
 const PageCom = () => {
+  const [activeTab, setActiveTab] = useState(tabData[0].name);
+
+  const handleTabChange = (selectedTab: string) => {
+    setActiveTab(selectedTab);
+  };
+
   return (
     <>
-      <Day_calls />
+      <section className={`${style.maincontainer}`}>
+        <div className={`${style.hedaingcontainer}`}>
+          <TabComponent activeKey={activeTab} tabOptions={tabData} onChangeTab={handleTabChange} />
+        </div>
+        <div>
+          <h1>asd</h1>
+        </div>
+        <div className={`${style.daycallcontainer}`}>
+          {activeTab === 'equity_trading' && <Equity_trading />}
+          {activeTab === 'option_trading' && <Option_trading />}
+          {activeTab === 'mcx' && <Equity_trading />}
+        </div>
+      </section>
     </>
   );
 };
