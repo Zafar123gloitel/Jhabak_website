ea24029384220af3a4bd37ff593955c673058d8c
rishabh <rishabh.nirmalkar@gloitel.in>
2024-04-04 19:14:40 +0530

    responsive the daycalls

--- src/app/(client)/client/layout.tsx
+++ src/app/(client)/client/layout.tsx
@@ -1,9 +1,11 @@
 'use client';
 import React from 'react';
 import '@/app/globals.scss';
+import { usePathname } from 'next/navigation';
 
 export default function ClientInfromation({ children }: { children: React.ReactNode }) {
   // const toggle = useAppSelector(selectToggle);
+  const pathname = usePathname();
 
   return (
     <>
@@ -15,7 +17,9 @@ export default function ClientInfromation({ children }: { children: React.ReactN
               <Sidebar isToggle={isToggle} setIsToggle={() => setIsToggle(!isToggle)} />
             </div> */}
           </>
-          <div className={`data_section inner_plans_tabs full_width`}>{children}</div>
+          <div className={`data_section inner_plans_tabs full_width ${pathname.startsWith('/client') ? 'w-100' : ''}`}>
+            {children}
+          </div>
         </div>
       </div>
     </>
